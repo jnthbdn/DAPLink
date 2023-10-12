e0ef4d3310e4e2c4a0f0bb07f50a4e4b931ace0b
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-10-12 18:12:32 +0200

    Implement w25q64 driver

--- source/board/steami/w25q64.c
+++ source/board/steami/w25q64.c
@@ -1,29 +1,7 @@
 #include "w25q64.h"
 #include "steami_spi.h"
 
-uint8_t w25q64_read_device_id(){
-    uint8_t buffer[5] = {0xAB, 0x00, 0x00, 0x00, 0x00};
-    
-    spi_steami_transfer_receive(buffer, buffer, 5, 1000);
-
-    return buffer[4];
-}
-
-uint8_t w25q64_read_manufacturer_id(){
-    uint8_t buffer[6] = {0x90, 0x00, 0x00, 0x00, 0x00, 0x00};
-
-    spi_steami_transfer_receive(buffer, buffer, 6, 1000);
-
-    return buffer[4];
-}
-
-uint8_t w25q64_read_status(){
-    uint8_t buffer[2] = {0x35, 0x00};
-
-    spi_steami_transfer_receive(buffer, buffer, 2, 1000);
-
-    return buffer[1];
-}
+#include "string.h"
 
 bool w25q64_init(){
     __HAL_RCC_GPIOB_CLK_ENABLE();
@@ -42,4 +20,118 @@ bool w25q64_init(){
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
 
     return w25q64_read_device_id() == 0x16;
+}
+
+void w25q64_write_enable(){
+    uint8_t cmd[1] = {0x06};
+    spi_steami_transfer_receive(cmd, cmd, 1, 1000);
+}
+
+uint8_t w25q64_read_device_id(){
+    uint8_t buffer[5] = {0xAB, 0x00, 0x00, 0x00, 0x00};
+    
+    spi_steami_transfer_receive(buffer, buffer, 5, 1000);
+
+    return buffer[4];
+}
+
+uint8_t w25q64_read_manufacturer_id(){
+    uint8_t buffer[6] = {0x90, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+    spi_steami_transfer_receive(buffer, buffer, 6, 1000);
+
+    return buffer[4];
+}
+
+bool w25q64_is_busy(){
+    uint8_t status = w25q64_read_status_register_1();
+    return (status & 0x01) > 0;
+}
+
+bool w25q64_is_WEL(){
+    uint8_t status = w25q64_read_status_register_1();
+    return (status & 0x02) > 0;
+}
+
+uint8_t w25q64_read_status_register_1(){
+    uint8_t status[2] = {0x05, 0x00};
+
+    spi_steami_transfer_receive(status, status, 2, 1000);
+
+    return status[1];
+}
+
+uint8_t w25q64_read_status_register_2(){
+    uint8_t status[2] = {0x35, 0x00};
+
+    spi_steami_transfer_receive(status, status, 2, 1000);
+
+    return status[1];
+}
+
+uint8_t w25q64_read_status_register_3(){
+    uint8_t status[2] = {0x15, 0x00};
+
+    spi_steami_transfer_receive(status, status, 2, 1000);
+
+    return status[1];
+}
+
+void w25q64_read_data(uint8_t* data, uint32_t address, uint16_t read_len){
+    uint16_t buffer_len = 4 + (uint16_t)read_len;
+
+
+    if( buffer_len > 260 ){ buffer_len = 260; }
+
+    uint8_t buffer[buffer_len];
+    memset(buffer, 0xFF, buffer_len);
+
+    buffer[0] = 0x03;
+    buffer[1] = (address & 0x00FF0000) >> 16;
+    buffer[2] = (address & 0x0000FF00) >> 8;
+    buffer[3] = address & 0x000000FF;
+
+    spi_steami_transfer_receive(buffer, buffer, buffer_len, 1000);
+    
+    memcpy(data, buffer + 4, read_len);
+}
+
+void w25q64_page_program(uint8_t* data, uint32_t address, uint16_t data_len){
+    uint16_t buffer_len = 4 + (uint16_t)data_len;
+
+    if( buffer_len > 260 ){ buffer_len = 260; }
+
+    uint8_t buffer[buffer_len];
+    memcpy(buffer+4, data, data_len);
+
+    buffer[0] = 0x02;
+    buffer[1] = (address & 0x00FF0000) >> 16;
+    buffer[2] = (address & 0x0000FF00) >> 8;
+    buffer[3] = address & 0x000000FF;
+
+    spi_steami_transfer(buffer, buffer_len, 1000);
+}
+
+void w25q64_sector_erase(uint32_t address){
+    uint8_t buffer[4] = { 0x20, (address & 0x00FF0000) >> 16, (address & 0x0000FF00) >> 8, address & 0x000000FF};
+
+    spi_steami_transfer(buffer, 4, 1000);
+}
+
+void w25q64_block_32k_erase(uint32_t address){
+    uint8_t buffer[4] = { 0x52, (address & 0x00FF0000) >> 16, (address & 0x0000FF00) >> 8, address & 0x000000FF};
+
+    spi_steami_transfer(buffer, 4, 1000);
+}
+
+
+void w25q64_block_64k_erase(uint32_t address){
+    uint8_t buffer[4] = { 0xD8, (address & 0x00FF0000) >> 16, (address & 0x0000FF00) >> 8, address & 0x000000FF};
+
+    spi_steami_transfer(buffer, 4, 1000);
+}
+
+void w25q64_chip_erase(){
+    uint8_t cmd = 0xC7;
+    spi_steami_transfer(&cmd, 1, 1000);
 }
--- source/board/steami/w25q64.h
+++ source/board/steami/w25q64.h
@@ -7,4 +7,72 @@ bool w25q64_init();
 
 uint8_t w25q64_read_device_id();
 uint8_t w25q64_read_manufacturer_id();
-uint8_t w25q64_read_status();
+
+/**
+ * @brief BUSY is a read only bit in the status register (S0) that is set to a 1 state when the device is executing a Page Program, Sector Erase, Block Erase, Chip Erase, Write Status Register or Erase/Program Security Register instruction. During this time the device will ignore further instructions except for the Read Status Register and Erase/Program Suspend instruction. When the program, erase or write status/security register instruction has completed, the BUSY bit will be cleared to a 0 state indicating the device is ready for further instructions.
+ * 
+ * @return true if BUSY flag is set, false otherwise
+ */
+bool w25q64_is_busy();
+
+/**
+ * @brief Write Enable Latch (WEL) is a read only bit in the status register (S1) that is set to 1 after executing a Write Enable Instruction. The WEL status bit is cleared to 0 when the device is write disabled. A write disable state occurs upon power-up or after any of the following instructions: Write Disable, Page Program, Quad Page Program, Sector Erase, Block Erase, Chip Erase, Write Status Register, Erase Security Register and Program Security Register.
+ * 
+ * @return true if WEL flag is set, false otherwise
+ */
+bool w25q64_is_WEL();
+
+uint8_t w25q64_read_status_register_1();
+uint8_t w25q64_read_status_register_2();
+uint8_t w25q64_read_status_register_3();
+
+/**
+ * @brief Write Enable Latch (WEL) is a read only bit in the status register (S1) that is set to 1 after executing a Write Enable Instruction. The WEL status bit is cleared to 0 when the device is write disabled. A write disable state occurs upon power-up or after any of the following instructions: Write Disable, Page Program, Quad Page Program, Sector Erase, Block Erase, Chip Erase, Write Status Register, Erase Security Register and Program Security Register.
+ * 
+ */
+void w25q64_write_enable();
+
+/**
+ * @brief The Read Data instruction allows one or more data bytes to be sequentially read from the memory.
+ * 
+ * @param data    array to store data
+ * @param address   address to start reading
+ * @param read_len  Number of byte to read (max. 256)
+ */
+void w25q64_read_data(uint8_t* data, uint32_t address, uint16_t read_len);
+
+/**
+ * @brief The Page Program instruction allows from one byte to 256 bytes (a page) of data to be programmed at previously erased (FFh) memory locations.
+ * 
+ * @param data the data pointer
+ * @param address the adresse to start writing (be carefull, if the end of the page is reach before the end of data, the address will wrap to the begin of the page)
+ * @param data_len Number of byte to write (max 256)
+ */
+void w25q64_page_program(uint8_t* data, uint32_t address, uint16_t data_len);
+
+/**
+ * @brief The Sector Erase instruction sets all memory within a specified sector (4K-bytes) to the erased state of all 1s (FFh).
+ * 
+ * @param address Address of sector to be deleted
+ */
+void w25q64_sector_erase(uint32_t address);
+
+/**
+ * @brief The Sector Erase instruction sets all memory within a specified sector (32K-bytes) to the erased state of all 1s (FFh).
+ * 
+ * @param address Address of sector to be deleted
+ */
+void w25q64_block_32k_erase(uint32_t address);
+
+/**
+ * @brief The Sector Erase instruction sets all memory within a specified sector (64K-bytes) to the erased state of all 1s (FFh).
+ * 
+ * @param address Address of sector to be deleted
+ */
+void w25q64_block_64k_erase(uint32_t address);
+
+/**
+ * @brief The Chip Erase instruction sets **all memory** within the device to the erased state of all 1s (FFh). WARNING: This operation can take from 20 to 100 seconds!
+ * 
+ */
+void w25q64_chip_erase();
