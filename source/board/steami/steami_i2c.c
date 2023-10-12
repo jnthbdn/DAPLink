b73bcd1965505078e0b024a95275397b5b47244b
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-10-12 18:17:10 +0200

    Improve I2C driver + Add on received callback

--- source/board/steami/steami_i2c.c
+++ source/board/steami/steami_i2c.c
@@ -5,14 +5,41 @@
 #include "stm32f1xx_hal_rcc.h"
 #include "stm32f1xx_hal_gpio.h"
 
-#include "stdint.h"
-#include "stdbool.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-#define I2C_BUFFER_SIZE 1024
+#define STEAMI_I2C_BUFFER_SIZE 256
+#define I2C_STEAMI_ADDRESS 0X76
 
-bool is_listen_I2C = false;
-I2C_HandleTypeDef* i2c_handle = NULL;
-uint8_t i2c_buffer[I2C_BUFFER_SIZE] = {0};
+typedef uint16_t (*steami_cmd_callback)(steami_i2c_command cmd, uint8_t* rx_data, uint16_t len_rx, uint8_t* tx_data);
+
+static bool is_listen_I2C = false;
+static I2C_HandleTypeDef* i2c_handle = NULL;
+static steami_cmd_callback on_cmd_recv = NULL;
+
+static uint8_t rx_i2c_command = 0x00;
+static uint8_t* rx_i2c_argument = NULL;
+static uint8_t rx_i2c_len_argument = 0;
+
+static uint8_t* tx_i2c_data = NULL;
+static uint8_t tx_i2c_len_data = 0;
+
+static bool is_command_valid(uint8_t cmd){
+    switch (cmd)
+    {
+        case WHO_AM_I:
+        case CLEAR_FLASH:
+        case SET_FILENAME:
+        case GET_FILENAME:
+        case WRITE_DATA:
+            return true;
+        
+        default:
+            return false;
+    }
+}
 
 void I2C1_EV_IRQHandler(void){
     HAL_I2C_EV_IRQHandler(i2c_handle);
@@ -54,23 +81,40 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef *hi2c){
 
 void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode)
 {
+    if( AddrMatchCode != I2C_STEAMI_ADDRESS ) return;
+
 	if(TransferDirection == I2C_DIRECTION_TRANSMIT) 
 	{
-        HAL_I2C_Slave_Seq_Receive_IT(hi2c, i2c_buffer, 1, I2C_FIRST_AND_LAST_FRAME);
+        rx_i2c_len_argument = 0;
+        HAL_I2C_Slave_Seq_Receive_IT(hi2c, &rx_i2c_command, 1, I2C_FIRST_FRAME);
 	}
 	else
 	{
-        HAL_I2C_Slave_Seq_Transmit_IT(hi2c, i2c_buffer, 1, I2C_FIRST_AND_LAST_FRAME);
+        if( tx_i2c_len_data == 0 ){
+            tx_i2c_data[0] = 0x00;
+            tx_i2c_len_data = 1;
+        }
+
+        // HAL_I2C_Slave_Seq_Transmit_IT(hi2c, tx_i2c_data, tx_i2c_len_data, I2C_FIRST_AND_LAST_FRAME);
+        HAL_I2C_Slave_Transmit(hi2c, tx_i2c_data, tx_i2c_len_data, 1000);
+        tx_i2c_len_data = 0;
 	}
+}
+
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+    HAL_I2C_Slave_Seq_Receive_IT(hi2c, &rx_i2c_argument[rx_i2c_len_argument], 1, I2C_FIRST_FRAME);
+    rx_i2c_len_argument++;
 }
 
 void HAL_I2C_ListenCpltCallback(I2C_HandleTypeDef *hi2c){
 
-    if( is_listen_I2C ) HAL_I2C_EnableListen_IT(hi2c);
-}
+    if( is_command_valid(rx_i2c_command) && on_cmd_recv != NULL ){
+        tx_i2c_len_data = on_cmd_recv( (steami_i2c_command)rx_i2c_command, rx_i2c_argument, rx_i2c_len_argument, tx_i2c_data);
+    }
 
-void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c){
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+    if( is_listen_I2C ) HAL_I2C_EnableListen_IT(hi2c);
 }
 
 void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c){
@@ -82,16 +126,21 @@ void steami_i2c_set_handler(I2C_HandleTypeDef *hi2c){
 }
 
 HAL_StatusTypeDef steami_i2c_init(){
+    rx_i2c_argument = (uint8_t*)malloc(STEAMI_I2C_BUFFER_SIZE * sizeof(uint8_t));
+    tx_i2c_data = (uint8_t*)malloc(STEAMI_I2C_MAX_TX_DATA * sizeof(uint8_t));
 
-    if( i2c_handle == NULL ){
+    if( i2c_handle == NULL || rx_i2c_argument == NULL || tx_i2c_data == NULL){
         return HAL_ERROR;
     }
+
+    memset(rx_i2c_argument, '\0', STEAMI_I2C_BUFFER_SIZE);
+    memset(tx_i2c_data, 0x00, STEAMI_I2C_MAX_TX_DATA);
 
     HAL_StatusTypeDef status;
 
     i2c_handle->Instance = I2C1;
     i2c_handle->Init.ClockSpeed = 100000;
-    i2c_handle->Init.OwnAddress1 = 0x76;
+    i2c_handle->Init.OwnAddress1 = I2C_STEAMI_ADDRESS;
     i2c_handle->Init.OwnAddress2 = 0x00;
     i2c_handle->Init.DutyCycle = I2C_DUTYCYCLE_2;
     i2c_handle->Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
@@ -132,4 +181,8 @@ HAL_StatusTypeDef steami_i2c_deinit(){
     is_listen_I2C = false;
     HAL_I2C_DisableListen_IT(i2c_handle);
     return HAL_I2C_DeInit(i2c_handle);
+}
+
+void steami_i2c_on_receive_command( steami_cmd_callback callback ){
+    on_cmd_recv = callback;
 }
--- source/board/steami/steami_i2c.h
+++ source/board/steami/steami_i2c.h
@@ -3,6 +3,23 @@
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_i2c.h"
 
+#define STEAMI_I2C_MAX_TX_DATA 12
+
+typedef enum  {
+    WHO_AM_I = 0x01,
+    CLEAR_FLASH = 0x02,
+    SET_FILENAME = 0x03,
+    GET_FILENAME = 0x04,
+    WRITE_DATA = 0x0A
+} steami_i2c_command;
+
 void steami_i2c_set_handler(I2C_HandleTypeDef *hi2c);
 HAL_StatusTypeDef steami_i2c_init();
 HAL_StatusTypeDef steami_i2c_deinit();
+
+/**
+ * @brief Set the function to be called each time a valid command is received (via I2C)
+ * 
+ * @param callback The callback returns the number of bytes to be returned (maximum data is defined by STEAMI_I2C_MAX_TX_DATA). `cmd` is the command received. `rx_data` contains the data received (without the command). `len_rx` is the number of bytes received. `tx_data` contains the data to be sent back to the master.
+ */
+void steami_i2c_on_receive_command( uint16_t(*callback)(steami_i2c_command cmd, uint8_t* rx_data, uint16_t len_rx, uint8_t* tx_data) );
