25f8e8abaf90a7ff91f8ee5f938b91d67b4fec13
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-07-02 15:46:34 +0200

    Disable TIM usage (use 30ms hooks daplink function)

--- source/board/steami/steami_tim.h
+++ source/board/steami/steami_tim.h
@@ -1,21 +1,21 @@
-#pragma once
+// #pragma once
 
-#include <stdbool.h>
-#include <stdint.h>
+// #include <stdbool.h>
+// #include <stdint.h>
 
-typedef void(*steami_tim_callback)(void);
+// typedef void(*steami_tim_callback)(void);
 
-/**
- * @brief Initialize TIM3
- * 
- * @param freq The frequency (in Hertz) at which the IRQ will be produced. Range: [1; 32767]. 
- * @return TRUE if successful, FALSE otherwise
- */
-bool steami_tim_init(uint32_t freq);
+// /**
+//  * @brief Initialize TIM3
+//  * 
+//  * @param freq The frequency (in Hertz) at which the IRQ will be produced. Range: [1; 32767]. 
+//  * @return TRUE if successful, FALSE otherwise
+//  */
+// bool steami_tim_init(uint32_t freq);
 
-/**
- * @brief Set the function called on Timer IRQ
- * 
- * @param cb the callback function (or NULL to disable)
- */
-void steami_tim_set_callback( steami_tim_callback cb );
+// /**
+//  * @brief Set the function called on Timer IRQ
+//  * 
+//  * @param cb the callback function (or NULL to disable)
+//  */
+// void steami_tim_set_callback( steami_tim_callback cb );
