8b37e86658da489a87f556889c920db688c0e1de
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-07-02 15:46:16 +0200

    Add LED functions

---
+++ source/board/steami/steami_led.h
@@ -0,0 +1,16 @@
+#pragma once
+
+void steami_led_init();
+
+void steami_led_toggle_green();
+
+void steami_led_turn_on_green();
+
+void steami_led_turn_off_green();
+
+void steami_led_toggle_blue();
+
+void steami_led_turn_on_blue();
+
+void steami_led_turn_off_blue();
+
